45e735820d25ccba6196321e16dc66b15c281ec2
Vinicius <vinicius.crisol@hotmail.com>
2020-10-13 15:47:44 -0300

    Fixing layout

--- web/src/styles/pages/CreatePost.ts
+++ web/src/styles/pages/CreatePost.ts
@@ -2,16 +2,24 @@ import styled from 'styled-components';
 import { Form as Unform } from '@unform/web';
 
 export const Container = styled.div`
-  max-width: 680px;
-  width: 100%;
-  padding: 1rem;
-  margin: 0 auto;
+  height: 100%;
+  padding: 2rem 1rem;
+  background: ${({ theme }) => theme.colors.gray};
 `;
 
 export const Form = styled(Unform)`
+  max-width: 680px;
+  width: 100%;
+
+  padding: 1rem;
+  margin: 0 auto;
+  border-radius: 4px;
+  background: ${({ theme }) => theme.colors.background};
+
+  font-size: 0.8rem;
+
   display: flex;
   flex-direction: column;
-  font-size: 0.8rem;
 
   > button {
     width: 50%;
@@ -21,8 +29,8 @@ export const Form = styled(Unform)`
 
 export const InputHeader = styled.div`
   padding: 0 4px;
-  flex-direction: column;
   display: flex;
+  flex-direction: column;
 
   & + input {
     margin-bottom: 0.5rem;
@@ -30,6 +38,6 @@ export const InputHeader = styled.div`
 
   span {
     opacity: 0.6;
-    margin: 2px 0;
+    margin: 2px 0 4px;
   }
 `;
